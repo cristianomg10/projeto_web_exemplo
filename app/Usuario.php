a2df866ff5f7a3ec5149b03a49b7574031747908
Cristiano Garcia <cristianooo@gmail.com>
2020-04-08 15:20:36 -0300

    .

--- app/Usuario.php
+++ app/Usuario.php
@@ -8,8 +8,5 @@ class Usuario extends Model
 {
     protected $table = 'usuarios';
     protected $primaryKey = 'id';
-
-    function vendas(){
-    	return $this->hasMany('App\Venda', 'id_usuario', 'id');
-    }
+ 
 }
